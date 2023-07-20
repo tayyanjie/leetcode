fd65c9fca0b314b7eb8e9cc7f0d3ebc0174d7af9
Yan Jie Tay <tayyanjie@gmail.com>
2023-07-20 13:58:24 +0800

    Add Solution for TwoSum

---
+++ solutions/array/TwoSum.py
@@ -0,0 +1,19 @@
+from abc import abstractmethod
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i in range(len(nums)):
+            num = nums[i]
+            if num in d:
+                d[num].append(i)
+            else:
+                d[num] = [i]
+        for i in range(len(nums)):
+            num = nums[i]
+            if target - num == num:
+                if len(d[num]) >= 2:
+                    return d[num][:2]
+            elif target - num in d:
+                return [i, d[target - num][0]]
