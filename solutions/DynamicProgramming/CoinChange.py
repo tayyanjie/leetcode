601edef10e1fd99fa5d76825b0a0dc572db1ff24
Yan Jie Tay <tayyanjie@gmail.com>
2023-07-24 19:22:45 +0800

    Add solution for Coin Change

---
+++ solutions/DynamicProgramming/CoinChange.py
@@ -0,0 +1,31 @@
+"""
+Solution of Evaluate Reverse Coin Change
+--------------------------------------------------------------------------------------------------------------
+https://leetcode.com/problems/coin-change/description/
+"""
+
+from typing import List
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # Memoization, alternatively use @cache to memoize
+        d = {0: 0}
+        total = amount
+
+        def helper(amount):
+            if amount in d:
+                return d[amount]
+            if amount < 0:
+                return total + 1
+            min_change = total + 1
+            for coin in coins:
+                min_change = min(min_change, helper(amount - coin) + 1)
+            d[amount] = min_change
+            return min_change
+
+        res = helper(amount)
+        if res > amount:
+            return -1
+        else:
+            return res
